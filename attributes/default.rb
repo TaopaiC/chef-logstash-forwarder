14cb50edf3df7553db0f204d8c5630d4137b19ad
TaopaiC <pctao.blog@pctao.org>
2014-07-14 15:47:08 +0800

    remove default attribute for hosts and files

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,13 +3,14 @@ default["logstash-forwarder"]["user"]                       = "logstash-forwarde
 default["logstash-forwarder"]["group"]                      = "logstash-forwarder"
 default["logstash-forwarder"]["dir"]                        = "/opt/logstash-forwarder"
 default["logstash-forwarder"]["log_dir"]                    = "#{node["logstash-forwarder"]["dir"]}/log"
-default["logstash-forwarder"]["hosts"]                       = [ "localhost" ]
+default["logstash-forwarder"]["hosts"]                       = nil
 default["logstash-forwarder"]["port"]                       = "6060"
 default["logstash-forwarder"]["timeout"]                    = "15"
 default["logstash-forwarder"]["ssl_certificate_path"]       = ""
 default["logstash-forwarder"]["ssl_key_path"]               = ""
 default["logstash-forwarder"]["ssl_ca_certificate_path"]    = ""
-default["logstash-forwarder"]["files"]                      = { "syslog" => [ '/var/log/syslog' ]}
+# default["logstash-forwarder"]["files"]                      = { "syslog" => [ '/var/log/syslog' ]}
+default["logstash-forwarder"]["files"]                      = nil
 default["logstash-forwarder"]["logstash_role"]              = "logstash"
 default["logstash-forwarder"]["logstash_fqdn"]              = ""
 default["logstash-forwarder"]["config_file"]                = "#{node["logstash-forwarder"]["dir"]}/logstash-forwarder.conf"
